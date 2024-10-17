256f29f62c6084dd3c490b847af63c7dea6cef2c
Tyler Collingridge <tylercollingridge@gmail.com>
2024-10-17 11:46:21 -0600

    feat: start creating mkdir command

--- mkdir_splitpath.c
+++ mkdir_splitpath.c
@@ -2,30 +2,49 @@
 
 #include "types.h"
 
-extern struct NODE* root;
-extern struct NODE* cwd;
+extern struct NODE *root;
+extern struct NODE *cwd;
 
-//make directory
-void mkdir(char pathName[]){
+// make directory
+void mkdir(char pathName[])
+{
+	// break up pathName into dirName (/a/b/c) and basename (d)
+	int LENGTH = strlen(pathName);
 
-    // TO BE IMPLEMENTED
-    //
-    // YOUR CODE TO REPLACE THE PRINTF FUNCTION BELOW
+	char *dirName = malloc(LENGTH * sizeof(char));
+	char *baseName = malloc(sizeof(char));
 
-    printf("TO BE IMPLEMENTED\n");
+	int i = 0;
+	// NOTE: for loop instead of while loop?
+	while (i != LENGTH)
+	{
+		dirName[i] = pathName[i]; // NOTE: may cause error - not sure if this is how to append to a char pointer
+		i++;
+	}
+	baseName = pathName[i]; // last element in pathName
 
-    return;
+	// search for dirname node:
+	//  - if absolute (/) start from root
+	//  else (relative) start from CWD (curr working dir)
+	//   - if dirname non-exist: ERROR message and return "FAIL"
+	//   - if exist but not DIR: ERROR msg and return "FAIL"
+
+	// search for basename (under dirname):
+	//  - if already exists: ERROR msg and return "FAIL"
+
+	//  add new dir node under dirname
+
+	// free memory?
+	return; // return success?
 }
 
-//handles tokenizing and absolute/relative pathing options
-struct NODE* splitPath(char* pathName, char* baseName, char* dirName){
+// handles tokenizing and absolute/relative pathing options
+struct NODE *splitPath(char *pathName, char *baseName, char *dirName)
+{
+	// TO BE IMPLEMENTED
+	// NOTE THAT WITHOUT COMPLETING THIS FUNCTION CORRECTLY
+	// rm, rmdir, ls, cd, touch COMMANDS WILL NOT EXECUTE CORRECTLY
+	// SEE THE PROVIDED EXECUTABLE TO SEE THEIR EXPECTED BEHAVIOR
 
-    // TO BE IMPLEMENTED
-    // NOTE THAT WITHOUT COMPLETING THIS FUNCTION CORRECTLY
-    // rm, rmdir, ls, cd, touch COMMANDS WILL NOT EXECUTE CORRECTLY
-    // SEE THE PROVIDED EXECUTABLE TO SEE THEIR EXPECTED BEHAVIOR
-
-    // YOUR CODE HERE
-    //
-    return NULL;
+	return NULL;
 }
